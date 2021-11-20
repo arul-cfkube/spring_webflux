145e746bde3071ea7e271e58b6cc5ed940fe8de5
ong <ong.develop@gmail.com>
2021-11-20 22:44:05 +0700

    learn webclient

--- src/test/java/com/ong/webfluxdemo/Lec01GetSingleResponseTest.java
+++ src/test/java/com/ong/webfluxdemo/Lec01GetSingleResponseTest.java
@@ -1,5 +1,7 @@
 package com.ong.webfluxdemo;
 
+import com.ong.webfluxdemo.dto.Response;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -8,5 +10,14 @@ public class Lec01GetSingleResponseTest extends BaseTest {
     @Autowired
     private WebClient webClient;
 
-
+    @Test
+    public void blockTest() {
+        Response response = this.webClient
+                .get()
+                .uri("reactive-math/square/{number}", 5)
+                .retrieve()
+                .bodyToMono(Response.class) // Mono<Response>
+                .block();
+        System.out.println(response);
+    }
 }
