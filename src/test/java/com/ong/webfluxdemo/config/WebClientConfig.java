1c82652f42c02481cf0b507ec8518b93f23a01f4
tester <example@email.com>
2021-11-21 13:15:58 +0700

    Learn about webclient with assignment

--- src/test/java/com/ong/webfluxdemo/config/WebClientConfig.java
+++ src/test/java/com/ong/webfluxdemo/config/WebClientConfig.java
@@ -2,7 +2,11 @@ package com.ong.webfluxdemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
@@ -11,6 +15,34 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080")
+                .filter(this::sessionToken)
+//                .defaultHeaders(h -> h.setBasicAuth("username", "password"))
+                .build();
+    }
+
+//    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction exchangeFunction) {
+//        System.out.println("generating session token");
+//        ClientRequest clientRequest = ClientRequest.from(request).headers(h -> h.setBearerAuth("some-lengthy-jwt")).build();
+//        return exchangeFunction.exchange(clientRequest);
+//    }
+
+    private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction exchangeFunction) {
+        // auth --> basic or auth
+        ClientRequest clientRequest = request.attribute("auth")
+                .map(v -> v.equals("basic") ? withBasicAuth(request) : withOAuth(request))
+                .orElse(request);
+        return exchangeFunction.exchange(clientRequest);
+    }
+
+    private ClientRequest withBasicAuth(ClientRequest request) {
+        return ClientRequest.from(request)
+                .headers(h -> h.setBasicAuth("username", "password"))
+                .build();
+    }
+
+    private ClientRequest withOAuth(ClientRequest request) {
+        return ClientRequest.from(request)
+                .headers(h -> h.setBearerAuth("some-token"))
                 .build();
     }
 }
