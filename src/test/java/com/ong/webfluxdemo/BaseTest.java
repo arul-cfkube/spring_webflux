a27a849cda4cd9fac4ab018fe24287cd1005e2ee
arulvannala <avannala@vmware.com>
2022-06-03 00:24:18 -0600

    test

--- src/test/java/com/ong/webfluxdemo/BaseTest.java
+++
@@ -1,9 +0,0 @@
-package com.ong.webfluxdemo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-class BaseTest {
-
-}
